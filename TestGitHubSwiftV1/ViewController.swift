98bdf0f26f604f02a11f9d4671bcac1a14de1b0c
Jon Moody <jmoodysr@gmail.com>
2018-11-12 18:25:39 -0800

    Initial Commit

---
+++ TestGitHubSwiftV1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGitHubSwiftV1
+//
+//  Created by JON MOODY1 on 11/12/18.
+//  Copyright © 2018 Jon Moody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
